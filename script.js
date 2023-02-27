422ad268fa3aacebff8576d0cfcc0fd7a787845e
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2023-02-27 20:38:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// popover working function form bootstrap
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+// gender select
+
+
+    const radioBtn1=document.querySelector('#flexRadioDefault1');
+    const radioBtn2=document.querySelector('#flexRadioDefault2');
+    const radioBtn3=document.querySelector('#flexRadioDefault3');
+    const genderSelect=document.querySelector('#genderSelect');
+
+    
+if(window.location.pathname === '/index.html'){
+  radioBtn1.addEventListener('change',()=>{
+    genderSelect.classList.add('d-none')
+  });
+  radioBtn2.addEventListener('change',()=>{
+    genderSelect.classList.add('d-none')
+  });
+  radioBtn3.addEventListener('change',()=>{
+    genderSelect.classList.remove('d-none')
+  });
+}
