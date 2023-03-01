28916886f22cf5f1e9c593b4860d70b8933f543d
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2023-03-01 14:19:03 +0530

    removing "if" condition to avoid conflict in sign up modal

--- script.js
+++ script.js
@@ -13,14 +13,12 @@ const radioBtn2 = document.querySelector("#flexRadioDefault2");
 const radioBtn3 = document.querySelector("#flexRadioDefault3");
 const genderSelect = document.querySelector("#genderSelect");
 
-if (window.location.pathname === "/") {
-  radioBtn1.addEventListener("change", () => {
-    genderSelect.classList.add("d-none");
-  });
-  radioBtn2.addEventListener("change", () => {
-    genderSelect.classList.add("d-none");
-  });
-  radioBtn3.addEventListener("change", () => {
-    genderSelect.classList.remove("d-none");
-  });
-}
+radioBtn1.addEventListener("change", () => {
+  genderSelect.classList.add("d-none");
+});
+radioBtn2.addEventListener("change", () => {
+  genderSelect.classList.add("d-none");
+});
+radioBtn3.addEventListener("change", () => {
+  genderSelect.classList.remove("d-none");
+});
