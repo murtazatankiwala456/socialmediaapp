0d91be00bc6b0ba24d4ce9bb93b31821da8e6911
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2023-03-01 13:35:44 +0530

    root file error fix

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-
 // popover working function form bootstrap
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
 
 // gender select
 
+const radioBtn1 = document.querySelector("#flexRadioDefault1");
+const radioBtn2 = document.querySelector("#flexRadioDefault2");
+const radioBtn3 = document.querySelector("#flexRadioDefault3");
+const genderSelect = document.querySelector("#genderSelect");
 
-    const radioBtn1=document.querySelector('#flexRadioDefault1');
-    const radioBtn2=document.querySelector('#flexRadioDefault2');
-    const radioBtn3=document.querySelector('#flexRadioDefault3');
-    const genderSelect=document.querySelector('#genderSelect');
-
-    
-if(window.location.pathname === '/index.html'){
-  radioBtn1.addEventListener('change',()=>{
-    genderSelect.classList.add('d-none')
+if (window.location.pathname === "/") {
+  radioBtn1.addEventListener("change", () => {
+    genderSelect.classList.add("d-none");
   });
-  radioBtn2.addEventListener('change',()=>{
-    genderSelect.classList.add('d-none')
+  radioBtn2.addEventListener("change", () => {
+    genderSelect.classList.add("d-none");
   });
-  radioBtn3.addEventListener('change',()=>{
-    genderSelect.classList.remove('d-none')
+  radioBtn3.addEventListener("change", () => {
+    genderSelect.classList.remove("d-none");
   });
 }
